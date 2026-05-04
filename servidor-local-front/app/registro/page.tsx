16e284edf1c7a1b3f4d2758391e1d5f833229a64
Gilson Monteiro <gilsonpatasmonteiro@gmail.com>
2026-05-04 12:36:19 -0100

    formulário registro

---
+++ servidor-local-front/app/registro/page.tsx
@@ -0,0 +1,17 @@
+
+import { LeftSectionRegistro } from "@/components/registro/left-section";
+import { RightSection } from "@/components/login/right-section";
+import { RightSectionRegistro } from "@/components/registro/right-section";
+
+export default function RegistroPage() {
+    return (
+    <div className="bg-gray-200 h-screen flex justify-between">
+       <LeftSectionRegistro/>
+
+       <RightSectionRegistro/>
+
+    </div>
+
+
+    );
+}
