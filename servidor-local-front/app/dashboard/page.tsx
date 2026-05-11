e332669a49afad91ae83d4b7614afe09917641d0
Gilson Monteiro <gilsonpatasmonteiro@gmail.com>
2026-05-11 09:33:53 -0100

    tbg Calendar

--- servidor-local-front/app/dashboard/page.tsx
+++ servidor-local-front/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-mimport UpcomingScheduleCard from "@/components/dashboard/upcoming-schedule-card"
+import UpcomingScheduleCard from "@/components/dashboard/upcoming-schedule-card"
 
-export default function dashboardpage() {
-    return (
-        <main className="p-8 bg-gray-200 min-h-screen">
-        <UpcomingScheduleCard/>
-        </main>
-    )
+export default function DashboardPage() {
+  return (
+    <main className="p-8 bg-gray-100 min-h-screen">
+      <UpcomingScheduleCard />
+    </main>
+  )
 }
--- servidor-local-front/components/dashboard/upcoming-schedule-card.tsx
+++ servidor-local-front/components/dashboard/upcoming-schedule-card.tsx
@@ -1,10 +1,44 @@
-import MMiniCalendar from "./mini-calendar"
-import appointmentItem from "./appointment-item"
+"use client"
+import MiniCalendar from "./mini-calendar"
+import AppointmentItem from "./appointment-item"
+import { Calendar } from "../ui/calendar"
+import { useState } from "react"
 
 export default function UpcomingScheduleCard() {
-    return (
-        <div className="w-[320px] rounded-2x1 border bg-white p-5 shadow-sm">
-<h2 className="text-lg front-semibold">
-    Upcoming Schedule
-    </h2>
-   ) }
+    const [date, setDate] = useState<Date | undefined>(new Date())
+  return (
+    <div className="w-[320px] rounded-2xl border bg-white p-5 shadow-sm">
+      <h2 className="text-lg font-semibold">
+        Upcoming Schedule
+      </h2>
+
+      <div className="w-full">
+        <Calendar className="w-full "selected={date} onSelect={setDate} mode="single" />
+      </div>
+
+      <div className="mt-6">
+        <p className="mb-4 font-medium text-gray-700">
+          Today, Oct 7
+        </p>
+
+        <div className="space-y-3">
+          <AppointmentItem
+            time="09:00 AM"
+            title="Door Repair"
+            address="124 Main St."
+          />
+
+          <AppointmentItem
+            time="01:30 PM"
+            title="Quote: Painting"
+            address="55 Oak Dr."
+          />
+        </div>
+      </div>
+
+      <button className="mt-6 w-full rounded-lg bg-sky-500 py-3 text-sm font-medium text-white">
+        Manage Calendar
+      </button>
+    </div>
+  )
+}
